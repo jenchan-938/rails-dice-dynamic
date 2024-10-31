a0727e053e1cb7732f2481c473aba37626b80320
jenchan-938 <jchan5@uchicago.edu>
2024-10-31 03:04:07 +0000

    10:04

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "home", :action => "options"})
 
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "dices", :action => "roll"})
+    
+
 end
